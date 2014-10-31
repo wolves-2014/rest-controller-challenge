a5e30c9d0dc232f9ae2a0bbaa3ec3a0a23576ffd
Patrick Grenning <pgrennin@gmail.com>
2014-10-30 22:08:12 -0500

    updated solution

--- app/controllers/addresses.rb
+++ app/controllers/addresses.rb
@@ -1,3 +1,18 @@
-get '/addresses' do
-  "this is the addresses page"
+get '/contacts/:contact_id/addresses/new' do
+  # "this is the addresses page"
+  @contact = Contact.find(params[:contact_id])
+  @address = Address.new
+  erb :"addresses/new"
 end
+
+post '/contacts/:contact_id/addresses' do
+  # @contact = Contact.find(params[:contact_id])
+  address = Address.create(params[:address])
+  address.update(contact_id: params[:contact_id])
+  # "Hello World #{params[:address]}"
+  # redirect '/contacts/' + '#{params[:contact_id]}'
+  redirect "/contacts/#{params[:contact_id]}"
+end
+
+# http://localhost:9393/contacts/3/addresses/new
+# contacts/2
